82dbc3a4db52c36303c1e79bae5c656ae3a77dd6
bpaner21 <bernardjohn.paner@loop.colum.edu>
2018-11-15 23:55:43 -0600

    Started making a scene loader script for Battle Win/ Game Over

---
+++ LargeTeam2018_TeamHeart_CombatPOC/Assets/SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadSceneOnClick(int sceneNo)
+    {
+        SceneManager.LoadScene(sceneNo);
+    }
+
+}
