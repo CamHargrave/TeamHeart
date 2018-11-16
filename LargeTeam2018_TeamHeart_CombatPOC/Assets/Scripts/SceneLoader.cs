5503488c714ec5ff33f9ba305df452cb71af255f
bpaner21 <bernardjohn.paner@loop.colum.edu>
2018-11-16 00:06:57 -0600

    made SceneLoader prefab and finished setting up Lose scene

--- LargeTeam2018_TeamHeart_CombatPOC/Assets/Scripts/SceneLoader.cs
+++ LargeTeam2018_TeamHeart_CombatPOC/Assets/Scripts/SceneLoader.cs
@@ -4,10 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    [SerializeField]
+    private int sceneNumber = 0;
 
-    public void LoadSceneOnClick(int sceneNo)
+    public void LoadSceneOnClick()
     {
-        SceneManager.LoadScene(sceneNo);
+        SceneManager.LoadScene(sceneNumber);
     }
 
 }
